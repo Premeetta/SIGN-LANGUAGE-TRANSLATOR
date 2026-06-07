624a7a7dbfe2149ef190a55afa9fa9c1a8424dee
amalbabu0 <amalbabu2803@gmail.com>
2026-06-07 21:11:49 +0530

    Serve static assets through Django on Vercel

--- MainProject/urls.py
+++ MainProject/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^static/(?P<path>.*)$', serve, {
+        'document_root': settings.BASE_DIR / 'Guest' / 'static',
+    }),
     path('Basics/',include('Basics.urls')),
     path('Admin/',include('Admin.urls')),
     path('',include('Guest.urls')),
